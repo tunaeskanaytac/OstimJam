5880e128444a20ad797f8074ad3efbbb54f82cf8
Tuna Eskan <tunaeskanaytac@gmail.com>
2025-05-03 19:40:06 +0300

    Enemy Attack Fix

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -39,7 +39,7 @@ public class PlayerController : MonoBehaviour
     private bool _isFacingRight = true;
     private bool _isRunning;
     public bool _canGetHurt = true;
-    private bool _isDying = false;
+    public bool _isDying = false;
     private bool _isClimbingLedge;
     #endregion
 
