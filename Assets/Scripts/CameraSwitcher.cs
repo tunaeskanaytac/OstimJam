ae9a713e1d1f94c4cce5c7c55443d7f52726c1c0
Berkay Tunca <berkaytunca40@gmail.com>
2025-05-04 13:25:24 +0300

    commit and oush

--- Assets/Scripts/CameraSwitcher.cs
+++ Assets/Scripts/CameraSwitcher.cs
@@ -10,11 +10,19 @@ public class CameraSwitcher : MonoBehaviour
 {
     [SerializeField] private CinemachineCamera camToEnable;
     [SerializeField] private CinemachineCamera camToDisable;
+    [SerializeField] private GameObject door;
+
+    private void Start()
+    {
+        camToDisable.Priority = 20;
+        camToEnable.Priority = 5;
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
+            door.SetActive(true);
             if (camToEnable != null && camToDisable != null)
             {
                 camToEnable.Priority = 20;
