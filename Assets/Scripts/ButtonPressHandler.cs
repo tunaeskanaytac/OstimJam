a208bc99fd652320b3d9e1eb93d061f9696e76fe
Tuna Eskan <tunaeskanaytac@gmail.com>
2025-05-04 18:11:43 +0300

    Add Main Menu

---
+++ Assets/Scripts/ButtonPressHandler.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonPressHandler : MonoBehaviour
+{
+    public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+        
+        // Check if the next scene exists in build settings
+        if (nextSceneIndex < SceneManager.sceneCountInBuildSettings)
+        {
+            SceneManager.LoadScene(nextSceneIndex);
+        }
+        else
+        {
+            Debug.LogWarning("No next scene available in build settings!");
+        }
+    }
+    
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+    }
+ 
+    
+}
